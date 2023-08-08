76702656e7f1a3d6fc95984f0855958568680387
제갈준 <138873994+jegaljune@users.noreply.github.com>
2023-08-08 13:45:15 +0900

    A vacation special lecture ( Java Script)
    처음 해본 자바 스크립트 입니다

---
+++ 자바 스크립트/1-변수할당값.js
@@ -0,0 +1,16 @@
+// Number (숫자)
+var Score = 90;
+
+//String (문자열)
+var text = '텍스트';
+
+//Boolean (참 혹은 거짓)
+var val = true;
+
+//null (빈 값)
+var empty = null;
+
+//undefined (지정되지 않음)
+var birth = undefined;
+
+console.log(text);

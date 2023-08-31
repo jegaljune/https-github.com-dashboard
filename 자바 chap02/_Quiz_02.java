ace55bc5affedf5397fcbcab1529abbca4873bec
제갈준 <138873994+jegaljune@users.noreply.github.com>
2023-08-31 15:30:16 +0900

    자바 chap02

---
+++ 자바 chap02/_Quiz_02.java
@@ -0,0 +1,10 @@
+package chap_02;
+
+public class _Quiz_02 {
+    public static void main(String[] args) {
+
+        int hight = 350;
+        String result = (hight >= 500) ? "헬창이 될수있습니다" : "헬창이 될수 없습니다";
+        System.out.println("3대 합이" + hight + "Kg 이므로 " + result);
+    }
+}
